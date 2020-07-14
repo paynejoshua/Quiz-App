dc1a837ad46dcbe29d4cea4665dc0ea82ad4efeb
Josh Payne <paynejoshua1@gmail.com>
2020-07-13 19:26:32 -0700

    updated html, JS, and css. Js now working. Still need to add a few things.

--- script.js
+++ script.js
@@ -1,67 +1,200 @@
-// Variables:
-    // Variable for start button (done)
-    // Variable for question area (done)
-    // Variable for answer buttons (done)
-    // Variable for final answer button (done)
-    // correct answer counter
-    // question counter to show which question we are on and how many are left (ex: 2/5, 3/5, etc.) => dispaly in top left corner
-    // variable for timer minutes and seconds
-
-    const startBtn = document.querySelector("#start-btn");
-    const qArea = document.querySelector("#questionArea");
-    const answerBtn = document.querySelector("#answer-buttons");
-    const finalBtn = document.querySelector("#final-btn");
-
-    let correct = 0;
-    let question = 0;
- 
+// Render new question and answer section:
+const timer = document.querySelector("#timer");
+const questions = document.querySelector("#questions");
+const answerBtn = document.querySelector("#answerBtn");
+const startBtn = document.querySelector("#startBtn");
+const answerA = document.querySelector("#answerA");
+const answerB = document.querySelector("#answerB");
+const answerC = document.querySelector("#answerC");
+const answerD = document.querySelector("#answerD");
+const finalScore = document.querySelector(".score");
+const choices = document.querySelectorAll(".choice");
 
 
-// Arrays: 
-    // 5 questions with 4 possible answers each
+// reveal question and answer buttons when clicking start
 
-// Functions: 
-    // remove hide from question area
-    // remove hide from final button
-    // conditionals to check if answer is correct
-    // When answer is incorrect subtract 5 seconds from clock
-    // countdown timer (display in top right corner) 
+startBtn.addEventListener("click", reveal)
+startBtn.addEventListener("click", beginCountdown)
 
-    function startTimer(duration, display) {
-        var timer = duration, minutes, seconds;
-        setInterval(function () {
-            minutes = parseInt(timer / 60, 10)
-            seconds = parseInt(timer % 60, 10);
-    
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-    
-            display.textContent = minutes + ":" + seconds;
-    
-            if (--timer < 0) {
-                timer = duration;
-            }
-        }, 1000);
-    }
-    
-    // change this to be onclick of start button
-   function beginCountDown() {
-        var tenMinutes = 60 * 10,
-            display = document.querySelector('#timer');
-        startTimer(tenMinutes, display);
-    };
+function reveal() {
+startBtn.classList.add("hide");
+questions.classList.remove("hide");
+answerBtn.classList.remove("hide");
+question1()
+}
 
-    startBtn.addEventListener("click", beginCountDown);
+function hide() {
+  questions.classList.add("hide");
+  answerBtn.classList.add("hide");
+}
 
 
-    // startBtn.addEventListener("click", startTimer);
+function question1() {
+
+for (i = 0; i < newQuestion.length; i++ ) {
+  questions.textContent =  newQuestion[0].question;
+
+  answerA.textContent = newQuestion[0].answer[0].text;
+  answerB.textContent = newQuestion[0].answer[1].text;
+  answerC.textContent = newQuestion[0].answer[2].text;
+  answerD.textContent = newQuestion[0].answer[3].text;
+
+
+}
+  answerA.addEventListener("click", question2);
+  answerB.addEventListener("click", question2);
+  answerC.addEventListener("click", question2);
+  answerD.addEventListener("click", question2);
+
+}
+
+
+function question2() {
+
+for (i = 0; i < newQuestion.length; i++ ) {
+  questions.textContent =  newQuestion[1].question;
+
+  answerA.textContent = newQuestion[1].answer[0].text;
+  answerB.textContent = newQuestion[1].answer[1].text;
+  answerC.textContent = newQuestion[1].answer[2].text;
+  answerD.textContent = newQuestion[1].answer[3].text;
+
+}
+answerA.addEventListener("click", question3);
+answerB.addEventListener("click", question3);
+answerC.addEventListener("click", question3);
+answerD.addEventListener("click", question3);
+
+}
+
+function question3() {
+
+for (i = 0; i < newQuestion.length; i++ ) {
+  questions.textContent =  newQuestion[2].question;
+
+  answerA.textContent = newQuestion[2].answer[0].text;
+  answerB.textContent = newQuestion[2].answer[1].text;
+  answerC.textContent = newQuestion[2].answer[2].text;
+  answerD.textContent = newQuestion[2].answer[3].text;
+
+}
+answerA.addEventListener("click", renderScores);
+answerB.addEventListener("click", renderScores);
+answerC.addEventListener("click", renderScores);
+answerD.addEventListener("click", renderScores);
+answerA.addEventListener("click", hide);
+answerB.addEventListener("click", hide);
+answerC.addEventListener("click", hide);
+answerD.addEventListener("click", hide);
+}
+
+
+// create array of questions with answers
+
+let newQuestion = [
+  {
+    question: "What does DOM stand for?",
+    answer: [
+     {text: "Document Object Model", correct: true},
+     {text: "Document Obvious Modality", correct: false},
+     {text: "Dont Offend the Mind", correct: false},
+     {text: "Delta Omni Mic", correct: false},
+    ]
+  },
+  {
+    question: "How do you prevent the browser from refreshing automatically?",
+    answer: [
+     {text: "Prevent Default", correct: true},
+     {text: "Prevent DOM", correct: false},
+     {text: "Stop Browser", correct: false},
+     {text: "Hold Query", correct: false},
+    ]
+  },
+  {
+    question: "How do you add an event listener for when the mouse is clicked?",
+    answer: [
+     {text: ".addeventlistener('click')", correct: true},
+     {text: ".Listenforclick", correct: false},
+     {text: ".clickbait", correct: false},
+     {text: ".clickityclickclic", correct: false},
+    ]
+  },
+];
+
+// begin countdown timer here:
+
+function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+      minutes = parseInt(timer / 60, 10)
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+          timer = duration;
+      }
+  }, 1000);
+}
+
+function beginCountdown() {
+  var fiveMinutes = 60 * 5,
+      display = document.querySelector('#timer');
+  startTimer(fiveMinutes, display);
+};
+
+
+// create conditional to check if answer correct is true if so then count towards score
+
+let quizScore = 0;
+
+
+// Score Input Section:
+var scoreInput = document.querySelector("#score-text");
+var scoreForm = document.querySelector("#score-form");
+var scoreList = document.querySelector("#score-list");
+
+
+var scores = [];
 
 
 
 
+function renderScores() {
+  // Clear scoreList element
+  scoreList.innerHTML = "";
+  finalScore.classList.remove("hide");
+
+  // Render a new li for each score
+  for (var i = 0; i < scores.length; i++) {
+    var score = scores[i];
+
+    var li = document.createElement("li");
+    li.textContent = score;
+    scoreList.appendChild(li);
+  }
+
+}
+
+// When form is submitted...
+scoreForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var scoreText = scoreInput.value.trim();
+
+  // Return from function early if submitted scoreText is blank
+  if (scoreText === "") {
+    return;
+  }
+
+  // Add new scoreText to scores array, clear the input
+  scores.push(scoreText);
+  scoreInput.value = "";
 
 
-
-// If I have time:
-    // See if there is a way to have background "breathe"
-    // Onclick of start button have a 10 sec countdown to with breathing instructions and "breathing" icon
+  // Re-render the list
+  renderScores();
+});
